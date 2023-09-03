cd65ad57c48077b13025166a5fc0c28423cc5fc0
lob23 <Hvblong21@apcs.fitus.edu.vn>
2023-09-03 11:59:53 +0700

    gradle update
    Kotlin option, java option to VERSION_17; Navigation dependencies, Camera_view dependency

---
+++ app/src/main/java/com/example/finalproject/SectionRoomScreen.kt
@@ -0,0 +1,11 @@
+package com.example.finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SectionRoomScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_section_room_screen)
+    }
+}
