62b5d3d956ec648a10f85f4dbfb7d2988e10f1ac
Nguyen Phuc Bao Uyen <hollyuyensan@gmail.com>
2023-09-08 23:51:42 +0700

    Merge branch 'main' of D:\Learning\Mobile\Final project\FinalMobile with conflicts.

--- app/src/main/java/com/example/finalproject/Home/HomeScreen.java
+++ app/src/main/java/com/example/finalproject/Home/HomeScreen.java
@@ -1,16 +1,28 @@
-package com.example.finalproject.Home;
+package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.finalproject.R;
+import com.yalantis.library.Koloda;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeScreen extends AppCompatActivity {
 
+    private SwipeAdater adapter;
+    private List<Integer> list;
+    Koloda koloda;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+
+        koloda = findViewById(R.id.HomeScreenSwipeItem);
+        list = new ArrayList<>();
+        adapter = new SwipeAdater(this, list);
+        koloda.setAdapter(adapter);
     }
 }
