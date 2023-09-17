ffd2f86692b72c9497ac894586306181ca5ff83c
Van Phuc Luu <hoktro18@gmail.com>
2023-09-17 11:29:11 +0700

    A little sign out

--- app/src/main/java/com/example/finalproject/FriendBio.java
+++ app/src/main/java/com/example/finalproject/FriendBio.java
@@ -87,6 +87,14 @@ public class FriendBio extends AppCompatActivity {
                     .document(currentFriend._UserName)
                     .update("_UserFriend", currentFriend._UserFriend );
 
+            // Delete conversation
+            String deleteId;
+            if( currentUser._UserName.compareTo(currentFriend._UserName) < 0 ) deleteId = currentUser._UserName + "+" + currentFriend._UserName;
+            else deleteId = currentFriend._UserName + "+" + currentUser._UserName;
+            database.collection(Constants.KEY_COLLECTION_CONVERSATION)
+                    .document(deleteId)
+                    .delete();
+
             // Return to Friend Screen
             Intent intent = new Intent(this, FriendsScreen.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
