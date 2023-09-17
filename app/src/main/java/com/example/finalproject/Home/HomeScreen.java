6e6a58d0bb48a3efea656ade31c661835e5586bd
Van Phuc Luu <hoktro18@gmail.com>
2023-09-18 02:16:07 +0700

    Update HomeScreen.java

--- app/src/main/java/com/example/finalproject/Home/HomeScreen.java
+++ app/src/main/java/com/example/finalproject/Home/HomeScreen.java
@@ -145,6 +145,8 @@ public class HomeScreen extends AppCompatActivity implements CardListener {
                     Position pos = null;
                     pos = positionfirebaseFirestoreController.retrieveObjectsFirestoreByID(FIRESTORE_LOCATION_KEY, user.get_UserName());
                     firestoreGeoHashQueries.QueryForLocationFireStore(user, pos, 500, activeUsers);
+                    activeUsers.removeAll(user.get_UserFriend());
+                    adapterSwipe.notifyDataSetChanged();
                 }
                 handler.post(new Runnable() {
 
