276c1dc48044f075bc07e4c3008541beb5bb6c44
tttrinhtran <89054365+tttrinhtran@users.noreply.github.com>
2023-09-18 20:10:01 +0700

    Update User.java

--- app/src/main/java/com/example/finalproject/User.java
+++ app/src/main/java/com/example/finalproject/User.java
@@ -318,7 +318,7 @@ public class User implements Serializable {
 
         for (String element : this._UserHobbies) {
             stringBuilder.append(element);
-            stringBuilder.append(" ");
+            stringBuilder.append("     ");
         }
 
 
