11f37a7e34561937a7c6d575d9b963bba3c9e522
Van Phuc Luu <hoktro18@gmail.com>
2023-09-18 14:03:09 +0700

    Nothing

--- app/src/main/java/com/example/finalproject/Home/HomeScreen.java
+++ app/src/main/java/com/example/finalproject/Home/HomeScreen.java
@@ -155,6 +155,7 @@ public class HomeScreen extends AppCompatActivity implements CardListener {
     @Override
     protected void onResume() {
         super.onResume();
+        startLocationService();
     }
 
     private void startLocationService() {
